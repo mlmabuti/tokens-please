a7b0a367d449f0df39966003c47aba08465f9575
axojolotl <axojolotl@protonmail.com>
2022-11-12 08:41:29 +0800

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import Footer from './components/Footer.jsx';
 import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 
-import './App.css'
-
 function App() {
   return (
-    <div>
+    <>
       <center>
         <Navbar />
         <Home />
         <Footer />
       </center>
-    </div>
+    </>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,20 +5,12 @@ function Home(){
     return (
         <div className="home">
             <Container className='container'>   
-
-                <h2>placeholder</h2>
-
+                <h2>TEST</h2>
                 <Alert severity='success'>{"Testing"}</Alert> 
-                {/* <Alert severity="error">This is an error alert — check it out!</Alert>
-                <Alert severity="warning">This is a warning alert — check it out!</Alert>
-                <Alert severity="success">This is a success alert — check it out!</Alert> */}
-
                 <TextField sx={{marginBottom:"1rem"}} id="outlined-basic" label="Enter single line code" variant="outlined" fullWidth />
                 <Button variant='contained'>Tokenizer</Button>
                 <Button variant='contained'>Parser</Button>
                 <Button variant='contained'>Semantic Analyzer</Button>
-
-
             </Container>
         </div>
     );
