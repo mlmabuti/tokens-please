94ea2196287fb93ed986395a318a2ead18b52886
axojolotl <axojolotl@protonmail.com>
2022-11-12 09:46:11 +0800

    adjust footer, plcadehold theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import Footer from './components/Footer.jsx';
 import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const Theme = createTheme({
+  palette: {
+    mode: 'light'
+  },
+});
+
 function App() {
   return (
     <>
+    <ThemeProvider theme={Theme}>
       <center>
         <Navbar />
         <Home />
         <Footer />
       </center>
+    </ThemeProvider>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,38 @@
 import './styles/Home.css'
-import {Alert, Button, Container, TextField} from '@mui/material/';
+import {Alert, Button, Container, TextField, Stack} from '@mui/material/';
+import TokenizerIcon from '@mui/icons-material/Toll';
+import ArrowDown from '@mui/icons-material/ArrowDownwardSharp';
+import ParserIcon from '@mui/icons-material/ManageSearch';
+import AnalyzerIcon from '@mui/icons-material/Spellcheck';
+
+const buttonAttributes = {
+    alignSelf: "center",
+    margin: ".5rem"
+}
+
 
 function Home(){
     return (
         <div className="home">
             <Container className='container'>   
+                <Stack>
                 <h2>TEST</h2>
-                <Alert severity='success'>{"Testing"}</Alert> 
-                <TextField sx={{marginBottom:"1rem"}} id="outlined-basic" label="Enter single line code" variant="outlined" fullWidth />
-                <Button variant='contained'>Tokenizer</Button>
-                <Button variant='contained'>Parser</Button>
-                <Button variant='contained'>Semantic Analyzer</Button>
+                
+                <Alert severity='info'>{"Testing"}</Alert> 
+
+                <TextField sx={{margin:"1rem"}} id="outlined-basic" label="Enter single line code" variant="outlined" />
+
+                <Button sx={buttonAttributes} variant='contained' startIcon={<TokenizerIcon/>}>Tokenizer</Button>
+
+                <ArrowDown sx={{alignSelf:"center"}}/>
+
+                <Button sx={buttonAttributes} variant='contained' startIcon={<ParserIcon/>}>Parser</Button>
+
+                <ArrowDown sx={{alignSelf:"center"}}/>
+
+                <Button sx={buttonAttributes} variant='contained' startIcon={<AnalyzerIcon/>}>Semantic Analyzer</Button>
+
+                </Stack>
             </Container>
         </div>
     );
